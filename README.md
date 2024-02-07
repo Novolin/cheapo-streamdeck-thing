1946ebef63222a6aa60f977718a787dc7d3ff9d2
Novolin <55156472+Novolin@users.noreply.github.com>
2024-02-07 23:00:40 +0000

    made it work good

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ It's just a few buttons and an LCD, made for my delightful wife :)
 
 
 Uses an aliexpress ssd1306-based OLED, RP2040 Zero and some switches and LEDs. 
+
+
+
+Images **MUST** be in 1 bit bmp format! 
--- scr.py
+++ scr.py
@@ -1,58 +1,118 @@
-import time
+import adafruit_imageload
 
+class Image:
+    # Bitmap image that can be passed to the screen display stuff
+    def __init__(self, file, size_x, size_y):
+        # It's only a black/white bmp so whatevs.
+        self.bitmap = adafruit_imageload.load(file)[0]
+        self.size_x = size_x
+        self.size_y = size_y
+    def draw_to_pos(self, screen, x_pos, y_pos):
+        # Draws the image to a given position on the screen
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                screen.pixel(x + x_pos,y + y_pos ,self.bitmap[x + (self.size_x * y)])
+    
+
+# The button that is shown on screen, just the display part
+class DisplayButton:
+    def __init__(self, text, position):
+        self.text = text[0:9] # truncate to 9 characters
+        if position == "left": #set the position for the text, this is where it will be centered
+            self.position = 3
+        else:
+            self.position = 67
+    def draw(self, do_fill, screen):
+        # blits the data to the screen buffer
+        # screen will require a refresh afterwards
+        screen.fill_rect(self.position, 52, 53, 10, do_fill)
+        x_pos = self.position + 60//len(self.text) # Center it nicely :)
+        screen.text(self.text, x_pos, 53, not do_fill)
+        
 
 class Screen:
     def __init__(self, screen_obj, left_button, right_button):
         # parent class for all screens,
         #renders the left and right buttons and their container squares
+        self.bg = Image("img/bg.bmp", 128, 64)
         self.screen = screen_obj
         self.left_button = left_button
         self.right_button = right_button
         self.blink_left = False
         self.blink_right = False
-        if len(left_button) > 10:
-            self.left_button = left_button[0:9]
-        if len(right_button) > 10:
-            self.right_button = right_button[0:9]
 
     def draw_screen_template(self):
-        self.screen.fill(0)
-        self.screen.rect(0, 0, 128, 64, 1)
-        self.screen.rect(3, 49, 58, 12, 1)
-        self.screen.rect(68, 49, 58, 12, 1)
-        self.screen.pixel(3, 49, 0)
-        self.screen.pixel(3, 60, 0)
-        self.screen.pixel(60, 49, 0)
-        self.screen.pixel(60, 60, 0)
-        self.screen.pixel(68, 49, 0)
-        self.screen.pixel(68, 60, 0)
-        self.screen.pixel(125, 49, 0)
-        self.screen.pixel(125, 60, 0)
-        self.screen.text(self.left_button, 6, 51, 1)
-        self.screen.text(self.right_button, 71, 51, 1)
+        self.bg.draw_to_pos(self.screen, 0,0)
+        self.draw_buttons()
         self.screen.show()
 
+    def draw_buttons(self):
+        self.left_button.draw(False, self.screen)
+        self.right_button.draw(False, self.screen)
     def do_blink(self, blink_state):
         if self.blink_left:
-            self.screen.rect(4, 50, 56, 10, blink_state)
-            self.screen.text(self.left_button, 6, 51, not blink_state)
+            self.left_button.draw(blink_state, self.screen)
         if self.blink_right:
-            self.screen.rect(4, 50, 56, 10, blink_state)
-            self.screen.text(self.right_button, 71, 51, not blink_state)
-
+            self.right_button.draw(blink_state, self.screen)
+    def left_action(self): # what to do when the left button is pressed, default scroll
+        return -1
+    def right_action(self): # what to do when the right button is pressed, default scroll
+        return 1
     def draw(self):
-        # Draw the content to the screen.
+        # Draw the content to the screen. changes per screen
         pass
 
 
-class ClockScreen(Screen):
+class MainScreen(Screen):
+    # Default screen, doesn't do a whole bunch
     def __init__(self, screen_obj):
-        super().__init__(screen_obj, "Previous", "Next")
-        self.current_time = time.localtime(time.time())
-        self.time_display = str(self.current_time.tm_hour)
-        #+ ":" + str(self.current_time.tm_min)
-        #+ ":" + str(self.current_time[5])
+        self.left_button = DisplayButton("Previous", "left")
+        self.right_button = DisplayButton("Next", "right")
+        super().__init__(screen_obj, self.left_button,  self.right_button)
+        self.img = Image("img/test.bmp", 124,46)
 
     def draw(self):
-        self.screen.text(self.time_display, 6, 20, 2)
+        self.img.draw_to_pos(self.screen, 2, 2)
         self.screen.show()
+    
+class StreamRunning(Screen):
+    def __init__(self, screen_obj, stream_obj):
+        self.stream = stream_obj
+        self.left_button = DisplayButton("Previous", "left")
+        self.right_button = DisplayButton("Next", "right")
+        super().__init__(screen_obj, self.left_button, self.right_button)
+    def draw(self):
+        self.screen.text("STREAMO", 6, 20, 1, size = 2)
+
+class CountScreen(Screen):
+    # Parent class for countdown screens
+    def __init__(self, screen_obj, text):
+        self.query_text = "Really " + text + "?"
+        self.confirm_count = 10
+        self.count_running = False
+        self.left_button = DisplayButton("Cancel", "left")
+        self.right_button = DisplayButton("Confirm", "right")
+        super().__init__(screen_obj, self.left_button, self.right_button)
+    
+    def start_countdown(self):
+        self.confirm_count = 10
+        self.left_button.blink = True
+        self.right_button.blink = True
+        self.count_running = True
+
+    def increment_count(self):
+        if self.count_running:
+            if self.confirm_count >= 0:
+                self.confirm_count -= 1 # Decrement the counter
+                return False
+            else: #cancel the countdown
+                return True
+    def draw(self):
+        pass
+
+class StreamPaused(Screen):
+    pass
+
+
+
+
